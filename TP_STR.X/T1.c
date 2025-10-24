69399b0b4f0f8a29aa1c696f83b57520ba8934fc
DBIBIH Oussama <dbibihossama@gmail.com>
2025-10-24 11:26:41 +0200

    update tache 4 and 5 and velocity T1

--- TP_STR.X/T1.c
+++ TP_STR.X/T1.c
@@ -1,90 +1,141 @@
 #include "T1.h"
 
-/**
- * @brief Tâche 1 - Affichage principal du tableau de bord.
- * @details Rafraîchit périodiquement l’état des capteurs et variables système.
- */
 void tache1(void)
 {
     unsigned char i;
 
-    di(); // Désactivation IT pendant init
+    di();
     initialisation_afficheur();
     clear_text();
     clear_graphics();
     init_rxtx();
     Init(SEM_RXTX);
-    RXTX_libre = 1;
-    TXREG1 = 'R';
-    ei(); // Réactivation IT
+    RXTX_libre=1;
+    TXREG1='R';
+    ei();
 
-    LED_R = LED_G = LED_B = 0;
-    vitesse = 0;
-    batterie = 120;
-    n_octet_badge = 0;
-    TP_appui = 0;
+    LED_R=0;LED_G=0;LED_B=0;
 
-    while (1)
+    vitesse=0;
+    batterie=120;
+    n_octet_badge=0;
+
+    goto_lico(13,34);draw_char('R');draw_char(' ');draw_char('V');draw_char(' ');draw_char('B');
+    goto_lico(14,34);draw_char('0');draw_char(' ');draw_char('0');draw_char(' ');draw_char('0');
+    goto_lico(15,34);draw_char('1');draw_char(' ');draw_char('1');draw_char(' ');draw_char('1');
+
+    TP_appui=0;
+
+    while(1)
     {
-        clear_text(); // rafraîchissement propre
 
-        // === Lignes de statut ===
-        goto_lico(0, 0);
-        draw_string("MODE : ");
-        if (MARCHE_AVANT == 0)      draw_string("AVANT");
-        else if (MARCHE_ARRIERE==0) draw_string("ARRIERE");
-        else                        draw_string("NEUTRE");
+        goto_lico(0,0);
+        draw_string("Marche:");
+        if (MARCHE_AVANT==0)
+            draw_string("AV");
+        else
+            if (MARCHE_ARRIERE==0)
+                draw_string("AR");
+            else
+                draw_string("N ");
 
-        goto_lico(1, 0);
-        draw_string("SIEGE : "); draw_char(SIEGE==0 ? '1':'0');
+        goto_lico(1,0);
+        draw_string("Siege:");
+        if (SIEGE==0)
+        {draw_char('1');}
+        else
+        {draw_char('0');}
 
-        goto_lico(2, 0);
-        draw_string("T_EAU : "); draw_hex8(lecture_8bit_analogique(TEMPERATURE_EAU));
+        goto_lico(2,0);
+        draw_string("Temp. Eau:");
+        draw_hex8(lecture_8bit_analogique(TEMPERATURE_EAU));
 
-        goto_lico(3, 0);
-        draw_string("T_HUILE : "); draw_hex8(lecture_8bit_analogique(TEMPERATURE_HUILE));
+        goto_lico(3,0);
+        draw_string("Temp. Huile:");
+        draw_hex8(lecture_8bit_analogique(TEMPERATURE_HUILE));
 
-        goto_lico(4, 0);
-        draw_string("CHOC : "); draw_char(CHOC==0 ? '1':'0');
+        goto_lico(4,0);
+        draw_string("Choc:");
+        if (CHOC==0)
+            draw_char('1');
+        else
+            draw_char('0');
 
+        // goto_lico(5,0);
+        // draw_string("Vitesse:");
+        // if (VITESSE_PLUS==0)
+        //     vitesse++;
+        // if (VITESSE_MOINS==0)
+        //     vitesse--;
+        // draw_hex8(vitesse);
+        // === Vitesse ===
         goto_lico(5, 0);
-        draw_string("VITESSE : ");
-        if (VITESSE_PLUS == 0)  vitesse++;
-        if (VITESSE_MOINS == 0) vitesse--;
-        draw_dec8(vitesse);
+        draw_string((unsigned char*)"Vitesse:");
+        if (VITESSE_PLUS == 0 && vitesse < 255)
+            vitesse++;
+        if (VITESSE_MOINS == 0 && vitesse > 0)
+            vitesse--;
+        draw_hex8(vitesse);
 
-        goto_lico(6, 0);
-        draw_string("BATTERIE : ");
-        if (BATTERIE_PLUS == 0)  batterie++;
-        if (BATTERIE_MOINS == 0) batterie--;
-        draw_dec8(batterie);
-
-        goto_lico(7, 0);
-        draw_string("FREIN : ");
-        if (FREIN_A_MAIN == 0) draw_string("ACTIF");
-        else                   draw_string("OFF");
-
-        goto_lico(8, 0);
-        draw_string("BADGE : ");
-        if (n_octet_badge == 0) draw_string("AUCUN");
-        else for (i=0; i<n_octet_badge; i++) draw_hex8(badge[i]);
-
-        goto_lico(9, 0);
-        draw_string("JOYSTICK X:");
-        draw_dec8(lecture_8bit_analogique(JOYSTICK_X));
-
-        goto_lico(10, 0);
-        draw_string("JOYSTICK Y:");
-        draw_dec8(lecture_8bit_analogique(JOYSTICK_Y));
-
-        if (TP_appui == 1)
+        if (vitesse > 30)
         {
-            goto_lico(12, 0);
-            draw_string("TOUCH x="); draw_dec8(TP_x);
-            draw_string(" y="); draw_dec8(TP_y);
-            plot1(TP_x, TP_y);
+            LED_R = 0; LED_G = 1; LED_B = 1;
+            goto_lico(6, 0);
+            draw_string((unsigned char*)"ALERTE: >30 km/h");
+        }
+        else
+        {
+            LED_R = 1; LED_G = 0; LED_B = 1;
+            goto_lico(6, 0);
+            draw_string((unsigned char*)"                 ");
+        }        
+
+        goto_lico(6,0);
+        draw_string("Batterie:");
+        if (BATTERIE_PLUS==0)
+            batterie++;
+        if (BATTERIE_MOINS==0)
+            batterie--;
+        draw_hex8(batterie);
+
+        goto_lico(7,0);
+        if (FREIN_A_MAIN==0)
+            draw_string("((!))");
+        else
+            draw_string("     ");
+
+        goto_lico(8,0);
+        draw_string("Badge:");
+        if (n_octet_badge==0)
+            draw_string(" AUCUN              ");
+        else
+        {
+            for (i=0;i<n_octet_badge;i++)
+            {
+                draw_hex8(badge[i]);
+            }
         }
 
-        for (i=0; i<10; i++) Nop(); // petit délai d’affichage
+        goto_lico(9,0);
+        draw_string("X-Joystick:");
+        draw_dec8(lecture_8bit_analogique(JOYSTICK_X));
+
+        goto_lico(10,0);
+        draw_string("Y-Joystick:");
+        draw_dec8(lecture_8bit_analogique(JOYSTICK_Y));
+
+        if (TP_appui==1)
+        {
+            goto_lico(0,20);
+            draw_string("x=");
+            draw_dec8(TP_x);
+            draw_string(" y=");
+            draw_dec8(TP_y);
+            plot1(TP_x,TP_y);
+        }
+        else
+        {
+            Nop();
+        }
     }
 }
--- TP_STR.X/T5.c
+++ TP_STR.X/T5.c
@@ -1,43 +1,22 @@
-// /**!
-//  * @file T5.c
-//  * @author DBIBIH Oussama
-//  * @brief Tache 5 : Envoi d'un message sur le port serie
-//  * @version 1.0
-//  */
-#include "T5.h"
-#include <stdlib.h>
-#include <stdio.h>
-
-// static const char message_tache5[] = "tache5_en_cours";
-
-// void tache5(void)
-// {
-//     unsigned int a;
-//     while(1)
-//     {
-//         rxtx_send_string(message_tache5);
-//         for (a = 0; a < 65000; a++)
-//             ;
-//         for (a = 0; a < 65000; a++)
-//             ;
-//     }
-// }
-
-#include "T5.h"
-
-/**
- * @brief Tâche 5 - Transmission périodique de la vitesse du véhicule.
+/**!
+ * @file T5.c
+ * @author DBIBIH Oussama
+ * @brief Tache 5 : Envoi d'un message sur le port serie
+ * @version 1.0
  */
+#include "T5.h"
+
+static const char message_tache5[] = "tache5_en_cours";
+
 void tache5(void)
 {
-    unsigned int a;
-    char buffer[32];
-
-    while (1)
-    {
-        sprintf(buffer, "VITESSE=%u\r\n", vitesse);
-        rxtx_send_string(buffer);
-
-        for (a=0; a<65000; a++); // délai d’envoi régulier
-    }
+    // unsigned int a;
+    // while(1)
+    // {
+    //     rxtx_send_string(message_tache5);
+    //     for (a = 0; a < 65000; a++)
+    //         ;
+    //     for (a = 0; a < 65000; a++)
+    //         ;
+    // }
 }
