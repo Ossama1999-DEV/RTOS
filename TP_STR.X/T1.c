b10bcab02afeb16ace4bf6ce409ca47480586d28
DBIBIH Oussama <dbibihossama@gmail.com>
2025-10-24 11:15:14 +0200

    update affiche and USB msg

--- TP_STR.X/T1.c
+++ TP_STR.X/T1.c
@@ -1,11 +1,14 @@
 #include "T1.h"
 
+/**
+ * @brief Tâche 1 - Affichage principal du tableau de bord.
+ * @details Rafraîchit périodiquement l’état des capteurs et variables système.
+ */
 void tache1(void)
 {
     unsigned char i;
 
-    // === Initialisation ===
-    di();
+    di(); // Désactivation IT pendant init
     initialisation_afficheur();
     clear_text();
     clear_graphics();
@@ -13,248 +16,75 @@ void tache1(void)
     Init(SEM_RXTX);
     RXTX_libre = 1;
     TXREG1 = 'R';
-    ei();
+    ei(); // Réactivation IT
 
-    LED_R = 0; LED_G = 0; LED_B = 0;
+    LED_R = LED_G = LED_B = 0;
     vitesse = 0;
     batterie = 120;
     n_octet_badge = 0;
     TP_appui = 0;
 
-    // === En-tête affichage ===
-    goto_lico(13, 34);
-    draw_char('R'); draw_char(' '); draw_char('V'); draw_char(' '); draw_char('B');
-    goto_lico(14, 34);
-    draw_char('0'); draw_char(' '); draw_char('0'); draw_char(' '); draw_char('0');
-    goto_lico(15, 34);
-    draw_char('1'); draw_char(' '); draw_char('1'); draw_char(' '); draw_char('1');
-
     while (1)
     {
-        // === Marche ===
+        clear_text(); // rafraîchissement propre
+
+        // === Lignes de statut ===
         goto_lico(0, 0);
-        draw_string((unsigned char*)"Marche:");
+        draw_string("MODE : ");
+        if (MARCHE_AVANT == 0)      draw_string("AVANT");
+        else if (MARCHE_ARRIERE==0) draw_string("ARRIERE");
+        else                        draw_string("NEUTRE");
 
-        if (MARCHE_AVANT == 0)
-        {
-            draw_string((unsigned char*)"AV");
-
-            // === Écran vert (marche avant) ===
-            LED_R = 1;  // OFF
-            LED_G = 0;  // ON
-            LED_B = 1;  // OFF
-
-            // Si ta lib gère la couleur de fond :
-            // set_color(0, 255, 0);    // vert
-            // clear_graphics();
-        }
-        else if (MARCHE_ARRIERE == 0)
-        {
-            draw_string((unsigned char*)"AR");
-
-            // === Écran jaune (marche arrière) ===
-            LED_R = 0;  // ON
-            LED_G = 0;  // ON
-            LED_B = 1;  // OFF
-
-            // Si ta lib gère la couleur de fond :
-            // set_color(255, 255, 0);  // jaune
-            // clear_graphics();
-        }
-        else
-        {
-            draw_string((unsigned char*)"N ");
-
-            // === Neutre ===
-            LED_R = 1;
-            LED_G = 1;
-            LED_B = 1;
-
-            // Si ta lib gère la couleur de fond :
-            // set_color(255, 255, 255); // blanc
-            // clear_graphics();
-        }
-
-        // === Siege ===
         goto_lico(1, 0);
-        draw_string((unsigned char*)"Siege:");
-        draw_char(SIEGE == 0 ? '1' : '0');
+        draw_string("SIEGE : "); draw_char(SIEGE==0 ? '1':'0');
 
-        // === Températures ===
         goto_lico(2, 0);
-        draw_string((unsigned char*)"Temp. Eau:");
-        draw_hex8(lecture_8bit_analogique(TEMPERATURE_EAU));
+        draw_string("T_EAU : "); draw_hex8(lecture_8bit_analogique(TEMPERATURE_EAU));
 
         goto_lico(3, 0);
-        draw_string((unsigned char*)"Temp. Huile:");
-        draw_hex8(lecture_8bit_analogique(TEMPERATURE_HUILE));
+        draw_string("T_HUILE : "); draw_hex8(lecture_8bit_analogique(TEMPERATURE_HUILE));
 
-        // === Choc ===
         goto_lico(4, 0);
-        draw_string((unsigned char*)"Choc:");
-        if (CHOC == 0)
-        {
-            draw_char('1');
-            LED_R = 0; LED_G = 1; LED_B = 1;
-            goto_lico(7, 0);
-            draw_string((unsigned char*)"!!! ALERTE CHOC !!!");
-        }
-        else
-        {
-            draw_char('0');
-            LED_R = 1; LED_G = 0; LED_B = 1;
-            goto_lico(7, 0);
-            draw_string((unsigned char*)"                   ");
-        }
+        draw_string("CHOC : "); draw_char(CHOC==0 ? '1':'0');
 
-        // === Vitesse ===
         goto_lico(5, 0);
-        draw_string((unsigned char*)"Vitesse:");
-        if (VITESSE_PLUS == 0 && vitesse < 255)
-            vitesse++;
-        if (VITESSE_MOINS == 0 && vitesse > 0)
-            vitesse--;
-        draw_hex8(vitesse);
+        draw_string("VITESSE : ");
+        if (VITESSE_PLUS == 0)  vitesse++;
+        if (VITESSE_MOINS == 0) vitesse--;
+        draw_dec8(vitesse);
 
-        if (vitesse > 30)
-        {
-            LED_R = 0; LED_G = 1; LED_B = 1;
-            goto_lico(6, 0);
-            draw_string((unsigned char*)"ALERTE: >30 km/h");
-        }
-        else
-        {
-            LED_R = 1; LED_G = 0; LED_B = 1;
-            goto_lico(6, 0);
-            draw_string((unsigned char*)"                 ");
-        }
+        goto_lico(6, 0);
+        draw_string("BATTERIE : ");
+        if (BATTERIE_PLUS == 0)  batterie++;
+        if (BATTERIE_MOINS == 0) batterie--;
+        draw_dec8(batterie);
 
-        // === Batterie ===
+        goto_lico(7, 0);
+        draw_string("FREIN : ");
+        if (FREIN_A_MAIN == 0) draw_string("ACTIF");
+        else                   draw_string("OFF");
+
         goto_lico(8, 0);
-        draw_string((unsigned char*)"Batterie:");
+        draw_string("BADGE : ");
+        if (n_octet_badge == 0) draw_string("AUCUN");
+        else for (i=0; i<n_octet_badge; i++) draw_hex8(badge[i]);
 
-        // Variation manuelle
-        if (BATTERIE_PLUS == 0)
-            batterie++;
-        if (BATTERIE_MOINS == 0 && batterie > 0)
-            batterie--;
-
-        // Alerte batterie faible
-        if (batterie < 30)
-        {
-            // === Écran rouge (batterie faible) ===
-            LED_R = 0;  // ON
-            LED_G = 1;  // OFF
-            LED_B = 1;  // OFF
-
-            // Si ta lib gère la couleur de fond :
-            // set_color(255, 0, 0); // rouge
-            // clear_graphics();
-
-            goto_lico(9, 0);
-            draw_string((unsigned char*)"!!! BATTERIE FAIBLE !!!");
-        }
-        else
-        {
-            // Rétablir couleurs normales
-            LED_R = 1;
-            LED_G = 0;  // vert ON
-            LED_B = 1;
-
-            // Effacer le message précédent
-            goto_lico(9, 0);
-            draw_string((unsigned char*)"                       ");
-        }
-
-        // Affichage du niveau
-        draw_hex8(batterie);
-
-
-        // === Frein à main ===
         goto_lico(9, 0);
-        draw_string((unsigned char*)"Frein:");
-
-        // Si le frein à main est serré
-        if (FREIN_A_MAIN == 0)
-        {
-            draw_string((unsigned char*)"((!))");
-
-            // === Écran rouge (alerte) ===
-            LED_R = 0;  // ON
-            LED_G = 1;  // OFF
-            LED_B = 1;  // OFF
-
-            // Si ta lib gère la couleur de fond :
-            // set_color(255, 0, 0);
-            // clear_graphics();
-
-            goto_lico(10, 0);
-            draw_string((unsigned char*)"ENLEVEZ FREIN A MAIN");
-
-            // Empêche la marche avant
-            if (MARCHE_AVANT == 0)
-            {
-                // Neutralise la commande
-                draw_string((unsigned char*)" - BLOQUE -");
-                vitesse = 0;  // sécurité : force l'arrêt
-            }
-        }
-        else
-        {
-            draw_string((unsigned char*)"     ");
-
-            // État normal : vert
-            LED_R = 1;
-            LED_G = 0;
-            LED_B = 1;
-
-            // Nettoie le message d’alerte
-            goto_lico(10, 0);
-            draw_string((unsigned char*)"                     ");
-        }
-
-
-        // === Badge ===
-        goto_lico(10, 0);
-        draw_string((unsigned char*)"Badge:");
-        if (n_octet_badge == 0)
-        {
-            draw_string((unsigned char*)" AUCUN");
-            goto_lico(11, 0);
-            draw_string((unsigned char*)"Inserez carte:");
-        }
-        else
-        {
-            // Nettoie proprement la ligne "Inserez carte:"
-            goto_lico(11, 0);
-            draw_string((unsigned char*)"               ");
-
-            for (i = 0; i < n_octet_badge; i++)
-                draw_hex8(badge[i]);
-        }
-
-        // === Joystick ===
-        goto_lico(12, 0);
-        draw_string((unsigned char*)"X-Joystick:");
+        draw_string("JOYSTICK X:");
         draw_dec8(lecture_8bit_analogique(JOYSTICK_X));
 
-        goto_lico(13, 0);
-        draw_string((unsigned char*)"Y-Joystick:");
+        goto_lico(10, 0);
+        draw_string("JOYSTICK Y:");
         draw_dec8(lecture_8bit_analogique(JOYSTICK_Y));
 
-        // === Touch Panel ===
         if (TP_appui == 1)
         {
-            goto_lico(0, 20);
-            draw_string((unsigned char*)"x=");
-            draw_dec8(TP_x);
-            draw_string((unsigned char*)" y=");
-            draw_dec8(TP_y);
+            goto_lico(12, 0);
+            draw_string("TOUCH x="); draw_dec8(TP_x);
+            draw_string(" y="); draw_dec8(TP_y);
             plot1(TP_x, TP_y);
         }
-        else
-        {
-            Nop();
-        }
+
+        for (i=0; i<10; i++) Nop(); // petit délai d’affichage
     }
 }
--- TP_STR.X/T4.c
+++ TP_STR.X/T4.c
@@ -1,20 +1,41 @@
-/**!
- * @file T4.c
- * @author DBIBIH Oussama
- * @brief Tache 4 : Envoi d'un message sur le port serie
- * @version 1.0
- */
+// /**!
+//  * @file T4.c
+//  * @author DBIBIH Oussama
+//  * @brief Tache 4 : Envoi d'un message sur le port serie
+//  * @version 1.0
+//  */
+#include "T4.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+// static const char message_tache4[] = "TACHE4_EN_COURS";
+
+// void tache4(void)
+// {
+//     unsigned int a;
+//     while(1)
+//     {
+//         rxtx_send_string(message_tache4);
+//         for (a = 0; a < 65000; a++)
+//             ;
+//     }
+// }
+
 #include "T4.h"
 
-static const char message_tache4[] = "TACHE4_EN_COURS";
-
+/**
+ * @brief Tâche 4 - Envoi sur liaison série de l’état du capteur de choc.
+ */
 void tache4(void)
 {
     unsigned int a;
-    while(1)
+    char buffer[32];
+
+    while (1)
     {
-        rxtx_send_string(message_tache4);
-        for (a = 0; a < 65000; a++)
-            ;
+        sprintf(buffer, "CHOC=%d\r\n", (CHOC==0) ? 1 : 0);
+        rxtx_send_string(buffer);
+
+        for (a=0; a<50000; a++); // temporisation
     }
 }
